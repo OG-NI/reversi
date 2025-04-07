2da657a98e4621278a4fed9a63759f849e5feb58
OG-NI <in.neu@online.de>
2025-04-07 10:43:13 +0200

    separate send and recv functions

--- socket_connection.py
+++ socket_connection.py
@@ -5,11 +5,8 @@ import json
 import game
 import console_ui
 
-BUFSIZE = 1024
-
 
 class SocketConnection:
-    # TODO change to abstract subclass "UI"
     def __init__(self, ui: console_ui.ConsoleUI):
         self.socket = socket.socket()
         self.game = game.Game()
@@ -29,14 +26,14 @@ class SocketConnection:
         colors = ['b', 'w']
         self.color = random.choice(colors)
         colors.remove(self.color)
-        self.connection.send(colors[0].encode())
+        self.__send(colors[0])
 
         self.start()
 
     def join(self, ip, port):
         self.socket.connect((ip, port))
         self.connection = self.socket
-        self.color = self.connection.recv(BUFSIZE).decode()
+        self.color = self._recv()
         self.start()
 
     def start(self):
@@ -49,14 +46,18 @@ class SocketConnection:
                 try:
                     x, y = self.ui.input_move()
                     self.game.make_move(x, y)
-                    self.connection.send(json.dumps((x, y)).encode())
+                    self.__send((x, y))
                 except ValueError as error:
                     self.ui.output_message_and_wait(str(error))
             else:
-                x, y = json.loads(self.connection.recv(BUFSIZE).decode())
+                x, y = self._recv()
                 try:
                     self.game.make_move(x, y)
                 except ValueError:
                     pass  # ignore invalid moves made by opponent
 
-    # TODO send and receive in separate functions
+    def __send(self, data):
+        self.connection.send(json.dumps(data).encode())
+
+    def _recv(self):
+        return json.loads(self.connection.recv(1024).decode())
